22ba2ed4671311a0723244739c4a371c0da170ce
SoucekJ <31449690+SoucekJ@users.noreply.github.com>
2018-03-05 12:27:38 -0600

    Add files via upload

--- TextLab08st.java
+++ TextLab08st.java
@@ -1,3 +1,4 @@
+
 // TextLab08st.java
 // This is the student, starting file for the TextLab08 assignment.
 
@@ -37,12 +38,27 @@ class FullName
 	{
 		/*
 		 *  80 point precondition: The entered name consists of a First Name and a Last Name seprated by a single space.
-		 *
-		 * 100 point precondition: The entered name consists of a First Name, Last Name and any number of Middle Names.
+		 */
+		int spaceNum = 0;
+		String firstName;
+		int i = name.length() - 2;
+		do{
+			if(name.substring(i, i + 1).equals(" ") == true){
+				spaceNum = i;
+			}
+			else{
+				i--;
+			}
+		}while(spaceNum == 0); 
+		String lastName = name.substring(spaceNum + 1);
+		firstName = name.substring(0, spaceNum);
+		return lastName + ", " + firstName;
+		
+		 /* 100 point precondition: The entered name consists of a First Name, Last Name and any number of Middle Names.
 		 *                         NOTE: "any number" means it is possible not to have no Middle Name at all.
 		 *                         You can assume the user is not entering any extra spaces.
 		 */
-
+		
 
 
 
@@ -50,4 +66,3 @@ class FullName
 
 	}
 }
-
